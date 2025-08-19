4fb311149db2a08bcd8b94b016e6feaec769e656
Estthorpe <estthorpe@gmailcom>
2025-08-19 13:30:08 +0100

    initial setup

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title='Sentiment NLP Analyzer', page_icon='🧠', layout='centered')
+st.title('Product Review Sentiment Analyzer')
+st.write('Paste a review and, in later stages, this app will predict sentiment and explain why.')
